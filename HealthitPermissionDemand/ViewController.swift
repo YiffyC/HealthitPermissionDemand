46b095c66237e7e047e756e90a7a41d3dd0ef377
Bérenger Thévenet <berenger.thevenet@laposte.net>
2023-01-08 22:35:03 +0100

    Initial Commit

---
+++ HealthitPermissionDemand/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  HealthitPermissionDemand
+//
+//  Created by Bérenger on 08/01/2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
